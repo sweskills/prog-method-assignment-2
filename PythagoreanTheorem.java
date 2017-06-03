a78864de86a06a00cfd54667c46871530ee8b267
S <s>
2017-06-03 18:43:36 +0100

    {"types":"C","runs":3}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -15,12 +15,12 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		println("this program computes the square and of two numbers and produceits root");
 		double a = readDouble("enter a: ");
 		double b = readDouble("enter b: ");
-		double c = readDouble("enter c: ");
+		
 		//
 		double a1 = Math.pow(a,2);
 		double b1 = Math.pow(b,2);
 		double c1 = a1 + b1;
-		c = Math.sqrt(c1);
+		double c = Math.sqrt(c1);
 
 		//
 		println("this is the sqare root of:" + c1);
