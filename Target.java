fa0dc32fc0a6910639679d1dad028713fee314d1
S <s>
2017-06-03 08:00:08 +0100

    {"types":"C","runs":30}

--- Target.java
+++ Target.java
@@ -14,5 +14,19 @@ public class Target extends GraphicsProgram {
 	
 	public void run() {
 		
+		int HEIGHT = getHeight();
+		
+		int WIDTH = getWidth();
+		
+		int x = HEIGHT / 2;
+		
+		int y = WIDTH / 2;
+		
+		GOval innercircle = new GOval(x, y, 21.6, 21.6);
+		
+		add(innercircle);
+		
+		
+		
 	}
 }
