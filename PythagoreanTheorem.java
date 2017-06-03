b2909a4849f57e59ccba073c7600238d8c166508
S <s>
2017-06-02 19:22:43 -0700

    {"types":"C","runs":0}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -9,7 +9,23 @@
 import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
-	public void run() {
+	
+	
+	
+	public void run(){
+		
+	int a = readInt("Enter a");
+	
+	int b = readInt("Enter b");
+	
+	double c;
+
+		
+		c = Math.sqrt((a*a) + (b*b));
+		
+		System.out.println("The ansa is " +c);
 		/* You fill this in */
 	}
+	
 }
+
