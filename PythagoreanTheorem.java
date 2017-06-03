201cbc1fefe2cf652a6da66e0b36f8143d6a1803
S <s>
2017-06-03 18:47:44 +0100

    {"types":"C","runs":3}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -20,7 +20,7 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		double a1 = Math.pow(a,2);
 		double b1 = Math.pow(b,2);
 		double c1 = a1 + b1;
-		double c = Math.sqrt(c1);
+		Math.sqrt(c1);
 
 		//
 		println("this is the sqare root of:" + c1);
