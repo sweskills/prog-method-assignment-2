4b6e9b21fcefe65b89371bc347bc4d3a3bccec7b
S <s>
2017-06-03 07:42:15 +0100

    {"types":"C","runs":29}

--- Pyramid.java
+++ Pyramid.java
@@ -43,7 +43,7 @@ public class Pyramid extends GraphicsProgram {
 			
 			x = WIDTHOFBASE / 2;
 			
-			GRect Rect = new GRect(x, y, 30, 12);
+			GRect Rect = new GRect(x, y, 360, 360);
 			
 			add(Rect);
 		}
