57d56447c2391fb72137ab88b2fb5a6c1d46744c
S <s>
2017-06-04 07:54:00 +0100

    {"types":"C","runs":36}

--- Pyramid.java
+++ Pyramid.java
@@ -39,14 +39,14 @@ public class Pyramid extends GraphicsProgram {
 		
 		for (int i= 14; i>0; i--) {
 			
-			for (int j=0; j<14; j++) {
+		for (int j=0; j<14; j++) {
 				x = i * 12;
 				
 				y = j * 30;
 	
-				GRect Rect = new GRect(x, y, 30, 12);
+		GRect Rect = new GRect(x, y, 30, 12);
 					
-			    add(Rect);
+	    add(Rect);
 			    
 			}
 				
