28dc698944cb1cb27a43061737c5f54340942faa
S <s>
2017-06-04 16:59:41 +0100

    {"types":"C","runs":75}

--- Pyramid.java
+++ Pyramid.java
@@ -1,71 +1,43 @@
-/*
- * File: Pyramid.java
- * Name: 
- * Section Leader: 
- * ------------------
- * This file is the starter file for the Pyramid problem.
- * It includes definitions of the constants that match the
- * sample run in the assignment, but you should make sure
- * that changing these values causes the generated display
- * to change accordingly.
- */
 
 import acm.graphics.*;
-
 import acm.program.*;
-
-public class Pyramid extends GraphicsProgram {
-
-/* Defining the constants values */
-
-   private static final int BRICK_WIDTH = 30;
-
-   private static final int BRICK_HEIGHT = 12;
-
-   private static final int BRICKS_IN_BASE = 14;
-
-    
-
-/* Defining the position of the PYRAMID */
-
-   public void run() {
-
-       double x = getWidth() / 2-BRICK_WIDTH;
-
-       double y = getHeight() - BRICKS_IN_BASE * BRICK_HEIGHT;
-
-        
-
-/* For loop draws the current row then evaluates the x and y of the row beneath.*/
-
-       for (int i = 0; i < BRICKS_IN_BASE; i++) {
-
-           drawRow(x, y, i);
-
-           y += BRICK_HEIGHT;
-
-           x -= BRICK_WIDTH / 2;
-
-       }
-
-   }
-
-    
-
-/* Draw bricks in current row */
-
-   private void drawRow(double x, double y, int count) {
-
-       for (int i = 0; i <= count; i++) {
-
-           add(new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT));
-
-           x += BRICK_WIDTH;
-
-       }
-
+import java.awt.*;
  
-
-   }
-
+public class Pyramid extends GraphicsProgram {
+ 
+/** Width of each brick in pixels */
+    private static final int BRICK_WIDTH = 40;
+ 
+/** Height of each brick in pixels */
+    private static final int BRICK_HEIGHT = 12;
+ 
+/** Number of bricks in the base of the pyramid */
+    private static final int BRICKS_IN_BASE = 14;
+     
+/** New window size */
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 400;
+     
+    public void run() {
+         
+        int brick_level = 0;
+        int brick_order = 0;
+        int x, y;
+         
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT); // adapting the window
+         
+        // looping through each brick level
+        for (brick_level = 0; brick_level <= BRICKS_IN_BASE - 1; brick_level++) {
+             
+            // looping through each brick for a particular level
+            for (brick_order = 0; brick_order <= BRICKS_IN_BASE - brick_level - 1; brick_order++) {
+                 
+                x = (WINDOW_WIDTH - BRICK_WIDTH * (BRICKS_IN_BASE - brick_level)) / 2 + BRICK_WIDTH * brick_order;
+                y = WINDOW_HEIGHT - BRICK_HEIGHT * (brick_level + 1);
+                GRect rectangle = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+                add(rectangle);
+                 
+            }
+        }
+    }
 }
