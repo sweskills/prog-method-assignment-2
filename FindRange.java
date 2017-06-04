bed065625a893c59ee4c0df59d773d87b396e427
S <s>
2017-06-04 23:20:38 +0100

    {"types":"C","runs":240}

--- FindRange.java
+++ FindRange.java
@@ -16,11 +16,11 @@ public class FindRange extends ConsoleProgram {
 	
 		
 		println("this program output the smallest and largest number");
-		for (int i = 0; i <= 10; i++){
+		
 			int x = readInt("?");
 			
 				if (x != SENTINEL){
-					
+					for (int i = 0; i <= 10; i++){			
 				}
 				
 		}
