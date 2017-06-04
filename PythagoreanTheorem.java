f40683ceddee63091f21d0169bb980f187f53893
S <s>
2017-06-04 19:51:08 +0100

    {"types":"C","runs":119}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -14,17 +14,19 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		
 		println("Enter values to compute Pythagoras theorem");
 		
+		//the user is asked to input two integers a and b//
+		
 		int a = readInt("enter a: ");
 		
 		int b = readInt("enter b: ");
 		
-		a = a * a ;
+		a = a * a ; //a is raised to the power of 2 in other words the square of a// 
 		
-		b = b * b ;
+		b = b * b ; //b is raised to the power of 2 in other words the square of b//
 		
-		double c = Math.sqrt(a + b);
+		double c = Math.sqrt(a + b); //this computes the sum of the squares of a and b as the value for c//
 		
-		println("The answer is " + c + " ." );
+		println("The answer is " + c + " ." ); 
 		
 		
 	}
