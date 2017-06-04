5682aaafee17dba573489d46ec2b185f9bfc3236
S <s>
2017-06-04 16:54:29 +0100

    {"types":"C","runs":76}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -9,13 +9,21 @@
 import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
+	
 	public void run() {
+		
 		println("Enter values to compute Pythagoras theorem");
+		
 		int a = readInt("enter a: ");
+		
 		int b = readInt("enter b: ");
+		
 		a = a * a ;
+		
 		b = b * b ;
+		
 		double c = Math.sqrt(a + b);
+		
 		println("The answer is " + c + " ." );
 		
 		
