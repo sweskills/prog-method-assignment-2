4d0ad2c259beaa7f2103d883c170a4bb00a7ce22
S <s>
2017-06-04 18:52:31 +0100

    {"types":"C","runs":93}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -11,8 +11,20 @@ import acm.program.*;
 import java.awt.*;
 
 public class ProgramHierarchy extends GraphicsProgram {	
+	
+	private static final int HEIGHT = 50;
+	
+	private static final int WIDTH = 100;
+	
 	public void run() {
-		/* You fill this in. */
+		
+		x = (getWidth() - WIDTH) / 2;
+		
+		y = (getHeight() - HEIGHT) / 2;
+		
+		GRect Rect = new GRect(x, y, WIDTH, HEIGHT);
+		
+		add(Rect);
 	}
 }
 
