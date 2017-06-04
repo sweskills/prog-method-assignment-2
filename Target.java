e8f9e466eea82996a5192d3046ea182544b98675
S <s>
2017-06-04 20:28:59 +0100

    {"types":"C","runs":138}

--- Target.java
+++ Target.java
@@ -33,7 +33,7 @@ public class Target extends GraphicsProgram {
 		GOval midcircle = new GOval(i, j, 46.8, 46.8);
 		
 		midcircle.setFilled(false);
-		midcircle.setColor(Color.white);
+		midcircle.setFillColor(Color.white);
 		add(midcircle);
 		
 		double a = (getWidth() - 72) / 2;
