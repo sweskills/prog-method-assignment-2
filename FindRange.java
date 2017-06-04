76e608be665613f2ebe62546c7aef2d7e5bde42a
S <s>
2017-06-04 23:12:11 +0100

    {"types":"C","runs":237}

--- FindRange.java
+++ FindRange.java
@@ -9,7 +9,15 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
-	public void run() {
+		private static final int SENTINEL = 0;
+	
+	//
+		public void run() {
+	
+		int i = readInt("?");
+		println("this program output the smallest and largest number");
+		while (i != SENTINEL);
+				
 		/* You fill this in */
 	}
 }
