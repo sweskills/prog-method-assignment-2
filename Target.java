5f15fe6942a76b7bdc2526f0ae26e7c2bfd0beec
S <s>
2017-06-04 17:44:19 +0100

    {"types":"C","runs":174}

--- Target.java
+++ Target.java
@@ -22,7 +22,10 @@ public class Target extends GraphicsProgram {
 		
 		//
 		GOval spor = new GOval(60, 40, 80, 80);
-		
+		spor.setColor(Color.WHITE);
+		spor.setFillColor(Color.WHITE);
+		spor.setFilled(true);
+		spor.sendForward();
 		add (spor);
 		
 		//
