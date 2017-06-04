04a852cb4060f8804bf785bc0fca98fbf5c8da61
S <s>
2017-06-04 19:45:35 +0100

    {"types":"C","runs":118}

--- Hailstone.java
+++ Hailstone.java
@@ -9,16 +9,25 @@
 import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
+	
 	public void run() {
+		
 		println("Enter a number:");
+		
 		int n = readInt("enter n: ");
-		while (n > 1) {
-			if ((n % 2) == 0) {
+		
+		while (n > 1) { //this while loop runs as long as n is > 1 because the Hailstone sequence does not apply to integers = 1 and below// 
+			
+			if ((n % 2) == 0) { //if n is even, n is divided by 2//
+				
 				n = (n / 2) ;
+				
 				println("n is even, so i take half: " + n );
 				
-			}else if ((n % 2) == 1) {
+			}else if ((n % 2) == 1) { //if n is odd, n is multiplied by 3 and 1 is added to it//
+				
 				n = (3 * n) + 1 ;
+				
 				println("n is odd, so i make 3n + 1: " + n );
 			}
 		}
