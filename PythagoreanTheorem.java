bfc46117c8db9f1e1c5f47263769076177fe7edc
S <s>
2017-06-04 13:57:37 +0100

    {"types":"C","runs":30}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -12,9 +12,10 @@ import java.lang.Math;
 public class PythagoreanTheorem extends ConsoleProgram {	
 	public void run() {
 		println("Enter values to compute Pythagorean Theorem.");
-		int a = readInt("a: ");
-		int b = readInt("b: ");
-		int x = a + b;
-		println("x");
+		println("This program adds two integers.");
+		int n1 = readInt("Enter n1: ");
+		int n2 = readInt("Enter n2: ");
+		int total = n1 + n2;
+		println("The total is " + total + ".");
 	}
 }
