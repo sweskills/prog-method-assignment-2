8af3a1b1d100da6bf9e352166660fd3b257d3b8c
S <s>
2017-06-04 22:27:37 +0100

    {"types":"C","runs":205}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -17,7 +17,7 @@ public class ProgramHierarchy extends GraphicsProgram {
 		add (new GLabel("Program", 224, 58));
 		
 		//
-		add (new GRect (20, 150, 90, 40));
+		add (new GRect (20, 150, 120, 40));
 		add (new GLabel("GraphicsProgram", 44, 178));
 		/* You fill this in. */
 	}
