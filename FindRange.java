21b28a010a4c206bd887196be9d7d091b949d1ff
S <s>
2017-06-04 22:28:03 +0100

    {"types":"C","runs":65}

--- FindRange.java
+++ FindRange.java
@@ -32,14 +32,23 @@ public class FindRange extends ConsoleProgram {
 			if (val == Sentinel) {
 				break;
 			}
+			
+			//checks if the value is greater than the maximum value 
+			//and then assign it as the largest
 			if (val > max) {
 				max = val;
+				
+			//checks if the value is less than the minimum value
+			//and then assign it as the smallest
 			} else if (val < min) {
 				min = val;
 			}
 		}
 		
+		//prints out the smallest number 
 		println("smallest: " + min);
+		
+		//prints out the largest number
 		println("largest: " + max);
 		
 	}
