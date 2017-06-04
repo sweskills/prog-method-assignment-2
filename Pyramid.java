f671893402b74d5710f86aca3be9d9d52db22dd1
S <s>
2017-06-04 22:00:05 +0100

    {"types":"C","runs":164}

--- Pyramid.java
+++ Pyramid.java
@@ -39,7 +39,7 @@ public class Pyramid extends GraphicsProgram {
 				
 		y = (getHeight() - BRICK_HEIGHT) / 2;
 		
-		GRect Rect = new GRect(x, getHeight(), 30, 12);
+		GRect Rect = new GRect(0, getHeight(), 30, 12);
 					
 		add(Rect);
 			
