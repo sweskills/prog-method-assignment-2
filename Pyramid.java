743510d47cb8665712a3a8c9b8bb7907a4daa71f
S <s>
2017-06-04 16:54:28 +0100

    {"types":"C","runs":76}

--- Pyramid.java
+++ Pyramid.java
@@ -29,10 +29,6 @@ public class Pyramid extends GraphicsProgram {
 		
 		int totalrect = getWidth() / BRICK_WIDTH;
 		
-		int WIDTHOFBASE = (getWidth() / 2);
-				
-		int HEIGHT = (getHeight() / 2);
-		
 		int y;
 				
 		int x;
