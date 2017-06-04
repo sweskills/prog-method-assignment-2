9c3eb0d1d3235cfe978bc5695342d9edde5d4a39
S <s>
2017-06-04 22:08:29 +0100

    {"types":"C","runs":167}

--- Pyramids.java
+++ Pyramids.java
@@ -1,4 +1,19 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class Pyramids {
+public class Pyramids extends GraphicsProgram {
+	
+/** Width of each brick in pixels */
+	private static final int BRICK_WIDTH = 30;
+
+/** Width of each brick in pixels */
+	private static final int BRICK_HEIGHT = 12;
+
+/** Number of bricks in the base of the pyramid */
+	private static final int BRICKS_IN_BASE = 14;
+	
+	public void run() {
+		
+	}
 
 }
