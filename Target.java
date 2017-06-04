19e19abf65792721ef66d78df4d5aedf8cf63734
S <s>
2017-06-04 17:32:23 +0100

    {"types":"C","runs":164}

--- Target.java
+++ Target.java
@@ -14,10 +14,21 @@ public class Target extends GraphicsProgram {
 	public void run() {
 		
 		///
-		GOval spar = new GOval(40, 20, 100,100);
+		GOval spar = new GOval(40, 20, 120,120);
 		spar.setColor(Color.RED);
 		spar.setFillColor(Color.RED);
 		spar.setFilled(true);
 		add (spar);
+		
+		//
+		GOval spor = new GOval(60, 40, 80, 80);
+		spor.setColor(Color.WHITE);
+		spor.setFillColor(Color.WHITE);
+		spor.setFilled(true);
 	}
 }
+add (new GOval(60, 20, 120, 120));
+
+add (new GOval(80, 40, 80, 80));
+add (new GOval(100, 60, 40, 40));
+/* You fill this in. */
