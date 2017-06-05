43c06636381c834a3441b19f4f7b9c03f26291bb
S <s>
2017-06-05 22:05:57 +0100

    {"types":"C","runs":105}

--- Target.java
+++ Target.java
@@ -14,30 +14,31 @@ public class Target extends GraphicsProgram {
 	
 	public void run() {
 		
-		//conversion from inches to pixels
+		/*conversion from inches to pixels*/
 		double inchToPix = 72;
 		
-		//set radii and center coordinates
+		/*set radii and center coordinates*/
 		double outerRad = 1 * inchToPix;
 		double middleRad = 0.65 * inchToPix;
 		double innerRad = 0.30 * inchToPix;
 		double centerX = getWidth()/2;
 		double centerY = getHeight()/2;
 		
-		GOval outerOval = new GOval (centerX - outerRad, centerY - outerRad, 2 * outerRad, 2 * outerRad);
-		outerOval.setFilled(true);
-		outerOval.setColor(Color.RED);
-		add(outerOval);
+		/*draw the circles, add them to canvas and set their respective colors*/
+		GOval outerCircle = new GOval (centerX - outerRad, centerY - outerRad, 2 * outerRad, 2 * outerRad);
+		outerCircle.setFilled(true);
+		outerCircle.setColor(Color.RED);
+		add(outerCircle);
 		
-		GOval middleOval = new GOval (centerX - middleRad, centerY - middleRad, 2 * middleRad, 2 * middleRad);
-		middleOval.setFilled(true);
-		middleOval.setColor(Color.WHITE);
-		add(middleOval);
+		GOval middleCircle = new GOval (centerX - middleRad, centerY - middleRad, 2 * middleRad, 2 * middleRad);
+		middleCircle.setFilled(true);
+		middleCircle.setColor(Color.WHITE);
+		add(middleCircle);
 		
-		GOval innerOval = new GOval (centerX - innerRad, centerY - innerRad, 2 * innerRad, 2 * innerRad);
-		innerOval.setFilled(true);
-		innerOval.setColor(Color.RED);
-		add(innerOval);
+		GOval innerCircle = new GOval (centerX - innerRad, centerY - innerRad, 2 * innerRad, 2 * innerRad);
+		innerCircle.setFilled(true);
+		innerCircle.setColor(Color.RED);
+		add(innerCircle);
 		
 	}
 }
