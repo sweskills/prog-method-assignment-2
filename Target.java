8d6b426c8ec1ad50523baa14b3392409f1eaa8e1
S <s>
2017-06-05 17:35:38 +0100

    {"types":"C","runs":92}

--- Target.java
+++ Target.java
@@ -13,6 +13,28 @@ import java.awt.*;
 public class Target extends GraphicsProgram {	
 	public void run() {
 		
+		double inchToPix = 72;
+		
+		double outerRad = 1 * inchToPix;
+		double middleRad = 0.65 * inchToPix;
+		double innerRad = 0.30 * inchToPix;
+		double x = getWidth()/2;
+		double y = getHeight()/2;
+		
+		GOval outerOval = new GOval (x - outerRad, y - outerRad, 2 * outerRad, 2 * outerRad);
+		outerOval.setFilled(true);
+		outerOval.setColor(Color.RED);
+		add(outerOval);
+		
+		GOval middleOval = new GOval (x - middleRad, y - middleRad, 2 * middleRad, 2 * middleRad);
+		middleOval.setFilled(true);
+		middleOval.setColor(Color.WHITE);
+		add(middleOval);
+		
+		GOval innerOval = new GOval (x - innerRad, y - innerRad, 2 * innerRad, 2 * innerRad);
+		innerOval.setFilled(true);
+		innerOval.setColor(Color.RED);
+		add(innerOval);
 		
 	}
 }
