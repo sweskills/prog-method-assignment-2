4a399bf27ad0c4bc5c13ee73ea5d36be8f9fc1b3
S <s>
2017-06-05 22:48:06 +0100

    {"types":"C","runs":29}

--- Target.java
+++ Target.java
@@ -11,7 +11,31 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+	private static final double OUTER = 1.0;
+	private static final double MIDDLE = 0.65;
+	private static final double INNER = 0.3;
 	public void run() {
-		/* You fill this in. */
+	//
+		GOval p = new GOval (40,20,120,120);
+		p.setColor(Color.RED);
+		p.setFillColor (Color.RED);
+		p.sendToBack();
+		add(p);
+		
+		//
+		
+		GOval r = new GOval (60,40,80,80);
+		r.setColor(Color.WHITE);
+		r.setFillColor (Color.WHITE);
+		r.setFilled(true);
+		r.isVisible();
+		add(r);
+		
+		GOval s = new GOval (80,60,40,40);
+		s.setColor(Color.RED);
+		s.setFillColor (Color.RED);
+		s.setFilled(true);
+		add(s);
+		
 	}
 }
