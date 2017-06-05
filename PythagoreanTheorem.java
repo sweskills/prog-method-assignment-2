27110220d8b19da7a9b3faf4b30c36b30f298d68
S <s>
2017-06-05 21:47:17 +0100

    {"types":"C","runs":98}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -11,21 +11,21 @@ import java.lang.Math;
 
 public class PythagoreanTheorem extends ConsoleProgram {
 	
-	//runs the program
+	/*runs the program*/
 	public void run() {
 		
 		println("Enter values to compute Pythagorean Theorem.");
 		
-		//prompts the user to enter a value for side a
+		/*prompts the user to enter a value for side a*/
 		int a = readInt("a: ");
 		
-		//prompts the user to enter a value for side b
+		/*prompts the user to enter a value for side b*/
 		int b = readInt("b: ");
 		
-		//computes the value for side c i.e. add the squares of a & b and then take the square root
+		/*computes the value for side c i.e. add the squares of a & b and then take the square root*/
 		double result = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 		
-		//prints out the result
+		/*prints the result*/
 		println("c = " + result);
 	}
 }
