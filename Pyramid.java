0326903b928488ff5bdd90ac75e9498ecec51821
S <s>
2017-06-05 12:26:58 +0100

    {"types":"C","runs":9}

--- Pyramid.java
+++ Pyramid.java
@@ -12,20 +12,19 @@
  		double x = (getWidth() - BRICKS_IN_BASE * BRICK_WIDTH) / 2;
  		double y = getHeight() - BRICK_HEIGHT;
  		for (int row = 0; row < BRICKS_IN_BASE; row++) {
- 			drawRow(x, y, (BRICKS_IN_BASE - row));
+ 			createRow(x, y, (BRICKS_IN_BASE - row));
  			y -= BRICK_HEIGHT;
  			x += BRICK_WIDTH / 2;
  		}
 	}
-	private void drawRow(double x, double y, int bricks) {
+	private void createRow(double x, double y, int bricks) {
  		for (int i = 0; i < bricks; i++) {
- 			drawBrick((x + i * BRICK_WIDTH), y);
+ 			createBrick((x + i * BRICK_WIDTH), y);
 		}
 	}
 
-	private void drawBrick(double x, double y) {
+	private void createBrick(double x, double y) {
  		GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
- 		rect.setColor(Color.BLACK);
  		add(rect);
   	}
   }
