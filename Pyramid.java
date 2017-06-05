37ab06001ac9e1ba2e582af4c1503b782e77a60b
S <s>
2017-06-05 09:15:04 +0100

    {"types":"C","runs":2}

--- Pyramid.java
+++ Pyramid.java
@@ -8,18 +8,18 @@
   	private static final int BRICK_WIDTH = 30;
   	private static final int BRICK_HEIGHT = 12;
   	public void run() {
- 		/* You fill this in. */
+ 		 /* You fill this in. */
  		double x = (getWidth() - BRICKS_IN_BASE * BRICK_WIDTH) / 2;
  		double y = getHeight() - BRICK_HEIGHT;
  	for (int row = 0; row < BRICKS_IN_BASE; row++) {
- 			drawRow(x, y, BRICKS_IN_BASE - row);
+ 			drawRow(x, y, (BRICKS_IN_BASE - row));
  			y -= BRICK_HEIGHT;
  			x += BRICK_WIDTH / 2;
  		}
 	}
 	private void drawRow(double x, double y, int bricks) {
  		for (int i = 0; i < bricks; i++) {
- 			drawBrick(x + i * BRICK_WIDTH, y);
+ 			drawBrick((x + i * BRICK_WIDTH), y);
 		}
 	}
 
