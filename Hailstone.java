28578ea44ac822e82cb472bc9bf86e4fd5262bc4
S <s>
2017-06-05 15:45:32 +0100

    {"types":"C","runs":7}

--- Hailstone.java
+++ Hailstone.java
@@ -10,26 +10,34 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
-		int x = readInt("?");
+		/* The program computes the Hailstone's sequence of any number
+		 * entered by the user  
+		 */
+		
+		println("...");
+		println("Enter any number to get the Hailstone sequence: ");
+		int x = readInt();
 		int y = 0;
 		
 		while (x != 1) {
 			
 			if (x % 2 == 0) {
 				
-				println( x+ " is even, so i take half: " + x/2 );
+				println( "          " +x+ " is even, so i take half: " + x/2 );
 				x /= 2;
 				y++;
 				
 			}
 			else{
-				println( x+ " is odd, so i compute 3x + 1: " + (3 * x + 1) );
+				println( "          " +x+ " is odd, so i compute 3x + 1: " + (3 * x + 1) );
 				x = 3 * x + 1;
 				y++;
+				
+				println("...");
 			}
 			
 		}
+		println("...");
 		println ("It took " + y + " steps to arrive at 1");
 	}
 }
