016c179cd74a0344ed7c0618c1250a102ab450e0
S <s>
2017-06-05 15:59:04 -0700

    {"types":"C","runs":6}

--- FindRange.java
+++ FindRange.java
@@ -10,16 +10,30 @@ import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
 	public void run() {
-		println("Please enter a lidt of int values");
-		int n;
-		while(n!=0){
-	
-	n =  readInt("?: ");
-		}
-	
-	
-		
-	}
-	
-}
 
+		println("Please enter a list of int values and end with 0 (the sentinel) ");
+		int n1 =  readInt("First value: ");
+		
+		if(n1==0){
+			println("No you cant enter s3ntinel first");
+		}
+		
+		//break;
+	
+		println("Enter more numbers");
+		int n = readInt("? ");
+		while (n !=0){
+			
+		}
+		if (n == 0)
+			println("This is the S3ntinel");
+		
+		while(n !=0){
+			 n = readInt("? ");
+		} 
+		}
+}
+		
+		
+
+		
