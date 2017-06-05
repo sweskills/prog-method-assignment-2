1a3093d7bf1f29266d3c8f673e6401af7357e273
S <s>
2017-06-05 18:06:01 +0100

    {"types":"C","runs":1}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -1,7 +1,8 @@
 /*
  * File: PythagoreanTheorem.java
- * Name: 
- * Section Leader: 
+
+ * Name: Enoch Mbaebie
+ * Sweskills Facilitator: Azeez 
  * -----------------------------
  * This file is the starter file for the PythagoreanTheorem problem.
  */
@@ -11,5 +12,10 @@ import acm.program.*;
 public class PythagoreanTheorem extends ConsoleProgram {
 	public void run() {
 		/* You fill this in */
+		println ("Input Number for computation.");
+		int a = readInt ("a:");
+		int b = readInt ("b:");
+		double c = Math.sqrt (a * a + b * b);
+		println ("c:" + c);
 	}
 }
