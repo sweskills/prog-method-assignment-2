851daf106ef87b4719aa716b39b7f3bffda36d40
S <s>
2017-06-05 23:09:41 +0100

    {"types":"C","runs":38}

--- Target.java
+++ Target.java
@@ -16,7 +16,7 @@ public class Target extends GraphicsProgram {
 	private static final double INNER = 0.3;
 	public void run() {
 	//
-		GOval p = new GOval (60,40,120,120);
+		GOval p = new GOval (80,60,40,40);
 		p.setColor(Color.RED);
 		p.setFillColor (Color.RED);
 		p.sendToBack();
