36b250099c14b9fc7022d4d1559169130512de81
S <s>
2017-06-05 14:56:26 -0700

    {"types":"C","runs":1}

--- FindRange.java
+++ FindRange.java
@@ -11,15 +11,15 @@ import acm.program.*;
 public class FindRange extends ConsoleProgram {
 	public void run() {
 		println("Please enter a lidt of int values");
-	int n1 =  readInt("n1: ");
+		int n;
+		while(n!=0){
 	
-	int compute(n){
-		if(n%2==0)
-			n+/2;
-		else
-			
+	n =  readInt("?: ");
+		}
+	
+	
 		
 	}
-	}
+	
 }
 
