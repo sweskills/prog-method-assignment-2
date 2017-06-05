61bfb956cc51007fd64ff963447aed2b13977fe2
S <s>
2017-06-05 21:53:52 +0100

    {"types":"C","runs":98}

--- Hailstone.java
+++ Hailstone.java
@@ -10,42 +10,40 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	
-	//runs the program
+	/*runs the program*/
 	public void run() {
 		
-		//prompts the user to enter a number
-		int n = readInt("Enter a number: ");
+		/*prompts the user to enter a number*/
+		int x = readInt("Enter a number: ");
 		
-		//stores the number of steps it takes to reach 1
-		int m = 0;
+		/*stores the number of steps it takes to reach 1*/
+		int z = 0;
 		
-		//checks the condition when n is not equal to 1
-		while (n != 1) {
+		while (x != 1) {
 			
-			//checks if n is divisible by 2 without remainder
-			if (n % 2 == 0) {
+			/*checks if x is divisible by 2 without remainder*/
+			if (x % 2 == 0) {
 				
-				//it carries out this operation if true
-				println (n + " is even, so I take half: " + n/2);
+				println (x + " is even, so I take half: " + x/2);
 				
-				n = n / 2; //divides n by 2
+				x = x / 2; 
 				
-				m++; //increments the steps
+				z++; /*increments the steps*/
 				
 			} else {
 				
-				//otherwise it does this
-				println (n + " is odd, so I make 3n + l: " + (3 * n + 1));
+				/*otherwise this*/
+				println (x + " is odd, so I make 3n + l: " + (3 * x + 1));
 				
-				n = 3 * n + 1; //multiplies n with 3 and add 1
+				x = 3 * x + 1;
 				
-				m++; //the steps increase
+				z++; 
 				
 			}
 		}
 		
-		//prints out this when it reaches 1 signifying the end of the program
-		println("The process took " + m + " steps to reach 1");
+		/*prints this at the end of the program*/
+		println("The process took " + z + " steps to reach 1");
 	}
 }
 
