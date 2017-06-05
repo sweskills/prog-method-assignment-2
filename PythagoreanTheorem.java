1c498cdb044391aecf0cb41ec97c0f8efe2d9a6c
S <s>
2017-06-05 23:53:06 +0100

    {"types":"C","runs":15}

--- PythagoreanTheorem.java
+++ PythagoreanTheorem.java
@@ -14,7 +14,9 @@ public class PythagoreanTheorem extends ConsoleProgram {
 		double a = readDouble ("enter the value of a");
 		double b = readDouble ("enter the value of b");
 	
-			
+		System.out.print ("enter a");
+		a = ;
+		
 				
 	}
 }
