d0aee6723d82602dd14c5ba4f161300bbe169422
S <s>
2017-06-05 23:39:54 +0100

    {"types":"C","runs":56}

--- Target.java
+++ Target.java
@@ -11,7 +11,7 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {
-	private static final double OUTER = 1.0;
+	private static final double OUTER = 2.0;
 	private static final double MIDDLE = 0.65;
 	private static final double INNER = 0.3;
 	public void run() {
