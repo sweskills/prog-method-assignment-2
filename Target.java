955fd782bf05e1a4c6a52a3d8e3403f1fc50bba5
S <s>
2017-06-05 14:53:04 +0100

    {"types":"C","runs":2}

--- Target.java
+++ Target.java
@@ -11,7 +11,35 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+	
+	
+	private static int inch = 72;
+	//private static int 
+	
+	Boolean Pascaline;
+	
 	public void run() {
-		/* You fill this in. */
+		
+		
 	}
+	
+	public void firstCircle(){
+		double radius = inch;
+		double x = (getWidth()/2)- (radius/2);
+		double y = (getHeight()/2) -(radius/2);
+		
+		GOval myOval = new GOval (110,110,101,110);
+		
+	}
+	
+	public void secondCircle(){
+		
+		
+	}
+	
+	public void thirdCircle(){
+		
+		
+	}
+	
 }
