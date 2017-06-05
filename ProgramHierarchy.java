0a77130f4b71e4b6ea7f38a144b98d4aa8dae827
S <s>
2017-06-05 21:18:59 +0100

    {"types":"C","runs":27}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -12,7 +12,41 @@ import java.awt.*;
 
 public class ProgramHierarchy extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
+		add (new GRect(230,30,120,40));
+		add (new GLabel("Program",200,58));
+		
+		//
+		
+		add (new GRect(20,150,140,40));
+		add (new GLabel("GraphicsProgram",45,178));
+		
+		
+		//
+		
+		add (new GRect(230,150,130,40));
+		add (new GLabel("ConsoleProgram",249,178));
+		
+		
+		//
+		
+		add (new GRect(450,150,130,40));
+		add (new GLabel("DialogProgram",45,178));
+		
+		
+		//
+		
+		GLine first = new GLine (288.0,70.0,288.0,150.0); 
+		//
+		
+		GLine second = new GLine (288.0,70.0,288.0,150.0);
+		
+		//
+		
+		GLine third = new GLine (288.0,70.0,526.0,150.0);
+		add(third);
+		
+		
+		
 	}
 }
 
