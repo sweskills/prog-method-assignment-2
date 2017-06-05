7c62c447a8aa6d5a14570fdb281ad48020cef696
S <s>
2017-06-05 23:22:14 +0100

    {"types":"C","runs":50}

--- ProgramHierarchy.java
+++ ProgramHierarchy.java
@@ -38,7 +38,7 @@ public class ProgramHierarchy extends GraphicsProgram {
 		GLine first = new GLine (100.0,70.0,100.0,150.0); 
 		//
 		
-		GLine second = new GLine (288.0,70.0,288.0,150.0);
+		GLine second = new GLine (288.0,70.0,528.0,150.0);
 		
 		//
 		
