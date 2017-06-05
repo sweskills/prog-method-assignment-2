0283342385cf5ad1f788809f50fc1da280a83c54
S <s>
2017-06-05 21:06:23 +0100

    {"types":"C","runs":14}

--- Target.java
+++ Target.java
@@ -1,17 +1,32 @@
 /*
  * File: Target.java
- * Name: 
- * Section Leader: 
- * -----------------
- * This file is the starter file for the Target problem.
+ * Name: Enoch Mbaebie
+ * Sweskills Tech Talent Academy 
+ * -----------------------------
  */
 
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
-
 public class Target extends GraphicsProgram {	
-	public void run() {
 		/* You fill this in. */
+		private static final int PIXELS_PER_INCH = 72;
+		private static final double RADIUS_OUTER_CIRCLE = 1.0;
+		private static final double RADIUS_OUTER_CIRCLE = 0.65;
+		private static final double RADIUS_INNER_CIRCLE = 0.3;
+		public void run() {
+			double x = getWidth (/2);
+			double y = getHeight (/2 );
+		drawCircle (x, y, RADIUS_OUTER_CIRCLE * PIXELS_PER_INCH, Color,RED);
+		drawCircle (x, y, RADIUS_WHITE_CIRCLE * PIXELS_PER_INCH, Color,WHITE);
+		drawCircle (x, y, RADIUS_INNER_CIRCLE * PIXELS_PER_INCH, Color,RED);
 	}
+		private void drawCircle(double cx, double cy, double r, color c) {
+			double x = cx - r;
+			double y = cy - r;
+			GOval circle = new GOval(2*r, 2*r);
+			circle.setColor(c); 
+			circle.setfilled (true);
+			add (circle, x, y);
+,		}
 }
