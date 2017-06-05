43fd4e7ee26d740c0ca7b5d248961d12526cec5d
S <s>
2017-06-05 21:44:35 +0100

    {"types":"C","runs":98}

--- Target.java
+++ Target.java
@@ -10,28 +10,31 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class Target extends GraphicsProgram {	
+public class Target extends GraphicsProgram {
+	
 	public void run() {
 		
+		//conversion from inches to pixels
 		double inchToPix = 72;
 		
+		//set radii and center coordinates
 		double outerRad = 1 * inchToPix;
 		double middleRad = 0.65 * inchToPix;
 		double innerRad = 0.30 * inchToPix;
-		double x = getWidth()/2;
-		double y = getHeight()/2;
+		double centerX = getWidth()/2;
+		double centerY = getHeight()/2;
 		
-		GOval outerOval = new GOval (x - outerRad, y - outerRad, 2 * outerRad, 2 * outerRad);
+		GOval outerOval = new GOval (centerX - outerRad, centerY - outerRad, 2 * outerRad, 2 * outerRad);
 		outerOval.setFilled(true);
 		outerOval.setColor(Color.RED);
 		add(outerOval);
 		
-		GOval middleOval = new GOval (x - middleRad, y - middleRad, 2 * middleRad, 2 * middleRad);
+		GOval middleOval = new GOval (centerX - middleRad, centerY - middleRad, 2 * middleRad, 2 * middleRad);
 		middleOval.setFilled(true);
 		middleOval.setColor(Color.WHITE);
 		add(middleOval);
 		
-		GOval innerOval = new GOval (x - innerRad, y - innerRad, 2 * innerRad, 2 * innerRad);
+		GOval innerOval = new GOval (centerX - innerRad, centerY - innerRad, 2 * innerRad, 2 * innerRad);
 		innerOval.setFilled(true);
 		innerOval.setColor(Color.RED);
 		add(innerOval);
