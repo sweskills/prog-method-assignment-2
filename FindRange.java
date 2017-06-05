6c275bc5e6ad5ce29e486acdea6a13f82293ce9d
S <s>
2017-06-05 18:27:56 +0100

    {"types":"C","runs":19}

--- FindRange.java
+++ FindRange.java
@@ -6,11 +6,45 @@
  * This file is the starter file for the FindRange problem.
  */
 
-import acm.program.*;
 
-public class FindRange extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
-}
 
+		import acm.program.*;
+
+		public class FindRange extends ConsoleProgram {
+			private static final int Sentinel = 0;
+			public void run() {
+			println("This program is to find the largest and the smallest");
+			int min = 0;
+			int max = 0;
+			
+			int firstVal = readInt();
+			if(firstVal == Sentinel);{
+				println("The value entered is Invalid");
+				{
+			while(true){
+				
+				int val = readInt();
+				
+				if(val == Sentinel){
+				break;
+				
+			}
+			if( val > max){
+				max = val ;
+			
+			}else if (val < min){
+				min = val;
+			}
+			}
+			println("smallest num ;" +min);
+			
+			println("largest num :" +max);
+			
+			
+			
+			}
+
+			
+			}	
+			}
+			}
