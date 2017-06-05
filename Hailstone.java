7f8629ceed1d6146a59ed159cc4712c8c7ea1ded
S <s>
2017-06-05 05:23:16 +0100

    {"types":"C","runs":260}

--- Hailstone.java
+++ Hailstone.java
@@ -15,17 +15,17 @@ public class Hailstone extends ConsoleProgram {
 		
 		int times = 100;
 		int x = readInt ("Enter a number:");
-		int n ;
+		int n = x ;
 		//
 		for (int i = n; i < 1; i++){
-		if (x % 2 != 0 ){
-			println ("+ x is odd");
-			int n = (3*x) + 1;
-			println ("so I make 3n+1:" + n);
+			if (x % 2 != 0 ){
+				println ("+ x is odd");
+				int y = (3*x) + 1;
+				println ("so I make 3n+1:" + y);
 		}	else {
-			println ("+x is even");
-			int n = x / 2;
-			println ("so I take half:" + n);
+				println ("+x is even");
+				int y = x / 2;
+				println ("so I take half:" + y);
 			}
 			/* You fill this in */
 		}
