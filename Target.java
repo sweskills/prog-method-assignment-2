8aa24368b9922cfbfe95978e16f306962ecc6862
S <s>
2017-06-06 14:49:29 +0100

    {"types":"C","runs":20}

--- Target.java
+++ Target.java
@@ -17,24 +17,24 @@ public class Target extends GraphicsProgram {
 		 */
 		double inch2Pix = 72;
 		
-		double maxiDiameter = 2 * 1 * inch2Pix;
-		double mediDiameter = 2 * 0.65 * inch2Pix;
-		double miniDiameter = 2 * 0.30 * inch2Pix;
+		double maxiRadius = 1 * inch2Pix;
+		double mediRadius = 0.65 * inch2Pix;
+		double miniRadius = 0.30 * inch2Pix;
 		
 		double x = getWidth()/2;
 		double y = getHeight()/2;
 		
-		GOval maxiTarget = new GOval(x - maxiDiameter, y - maxiDiameter, maxiDiameter, maxiDiameter);
+		GOval maxiTarget = new GOval(x - maxiRadius, y - maxiRadius, 2 * maxiRadius, 2 * maxiRadius);
 		maxiTarget.setFilled(true);
 		maxiTarget.setColor(Color.RED);
 		add(maxiTarget);
 		
-		GOval mediTarget = new GOval(x - mediDiameter, y - mediDiameter, mediDiameter, mediDiameter);
+		GOval mediTarget = new GOval(x - mediRadius, y - mediRadius, 2 * mediRadius, 2 * mediRadius);
 		mediTarget.setFilled(true);
 		mediTarget.setColor(Color.WHITE);
 		add(mediTarget);
 		
-		GOval miniTarget = new GOval(x - miniDiameter, y - miniDiameter, miniDiameter, miniDiameter);
+		GOval miniTarget = new GOval(x - miniRadius, y - miniRadius, 2 * miniRadius, 2 * miniRadius);
 		miniTarget.setFilled(true);
 		miniTarget.setColor(Color.RED);
 		add(miniTarget);
