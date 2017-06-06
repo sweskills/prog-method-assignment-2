71f908cbf710fe6beb88ab6d4217a5f760511ab7
S <s>
2017-06-06 15:27:20 +0100

    {"types":"C","runs":0}

--- Target.java
+++ Target.java
@@ -11,7 +11,50 @@ import acm.program.*;
 import java.awt.*;
 
 public class Target extends GraphicsProgram {	
+		private double inch = 72;
+	
 	public void run() {
-		/* You fill this in. */
-	}
+		
+		firstCircle();
+		secondCircle();
+		thirdCircle();
+					
+		}
+	
+		public void firstCircle(){
+		double radius = inch * 1;
+		double x = (getWidth()/2) - radius/2;
+		double y = ((getHeight()/2) - radius/2);
+		
+		GOval firstOval = new GOval (x, y, radius, radius);
+		add(firstOval);
+		firstOval.setFilled(true);
+		firstOval.setFillColor(Color.RED);
+		}
+		
+		public void secondCircle() {
+		double radius = inch * 0.65;
+		double x = ((getWidth())/2) - radius/2;
+		double y = ((getHeight()/2) - radius/2);
+			
+		GOval secondOval = new GOval (x, y, radius, radius);
+		add(secondOval);
+		secondOval.setFilled(true);
+		secondOval.setColor(Color.WHITE);
+		secondOval.setFillColor(Color.WHITE);
+		}
+		
+		
+		public void thirdCircle() {
+		double radius = inch * 0.3;
+		double x = (getWidth()/2) - radius/2;
+		double y = ((getHeight()/2) - radius/2);
+		
+		GOval thirdOval = new GOval (x, y, radius, radius);
+		add(thirdOval);
+		thirdOval.setFilled(true);
+		thirdOval.setFillColor(Color.RED);
+		}
+		
+		
 }
