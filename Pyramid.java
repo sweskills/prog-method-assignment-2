5d0b136d19a9e646cda077a78e9785370f92911a
S <s>
2017-06-06 10:38:26 +0100

    {"types":"A","runs":17}

---
+++ Pyramid.java
@@ -0,0 +1,44 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+public class Pyramid extends GraphicsProgram {
+	/** Width of each brick in pixels */
+    private static final int BRICK_WIDTH = 30;
+
+/** Width of each brick in pixels */
+    private static final int BRICK_HEIGHT = 12;
+
+/** Number of bricks in the base of the pyramid */
+    private static final int BRICKS_IN_BASE = 14;
+    
+    public void run() {
+        /* You fill this in. */
+        double x = (getWidth() - BRICKS_IN_BASE * BRICK_WIDTH) / 2;
+        double y = getHeight() - BRICK_HEIGHT;
+        
+        for (int row = 0; row < BRICKS_IN_BASE; row++){
+            drawRow(x, y, (BRICKS_IN_BASE - row));
+            y -= BRICK_HEIGHT;
+            x += BRICK_WIDTH / 2;
+        }
+    }
+    
+    private void drawRow(double x, double y, int bricks){
+        for (int i = 0; i < bricks; i++){
+            drawBrick((x + i * BRICK_WIDTH), y);
+        }
+    }
+    private void drawBrick(double x, double y){
+        GRect rect = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
+        add(rect);
+}
+	   
+}      
+       
+        
+        
+       
+       
+       
+          
+   
