8d3ddaf22415acf60f8cf14fbd24e8f657938ba3
S <s>
2017-06-06 15:20:59 +0100

    {"types":"C","runs":25}

--- FindRange.java
+++ FindRange.java
@@ -10,7 +10,24 @@ import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
+		/* 
+		 * This program finds the range of 
+		 * any set of numbers until the Sentinel
+		 * value is reached 
+		 */
+		println("This program finds displays the minimum and");
+		println("maximum values in a given set of numbers");
+		
+			
+		println("Enter a value");
+		int val = readInt();
+		if (val == SENTINEL){
+			println("Number not valid");
+		}
+		
+		int min = 0;
+		int max = 0;
 	}
+	private static final int SENTINEL = 20;
 }
 
