b41d3cab87761af8a71ade2575d09a8d4c25f6cf
S <s>
2017-06-06 16:33:34 -0700

    {"types":"C","runs":2}

--- Pyramid.java
+++ Pyramid.java
@@ -9,24 +9,31 @@
  * that changing these values causes the generated display
  * to change accordingly.
  */
-
 import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
-
+ 
 public class Pyramid extends GraphicsProgram {
-
-/** Width of each brick in pixels */
-	private static final int BRICK_WIDTH = 30;
-
-/** Width of each brick in pixels */
-	private static final int BRICK_HEIGHT = 12;
-
-/** Number of bricks in the base of the pyramid */
-	private static final int BRICKS_IN_BASE = 14;
-	
-	public void run() {
-		/* You fill this in. */
-	}
+  private static final int BRICK_WIDTH = 30;
+  private static final int BRICK_HEIGHT = 12;
+  private static final int BRICKS_IN_BASE = 14;
+ 
+  public void run() {
+        putAllBricks();
+    }
+    private void putAllBricks()
+    {
+        for( int row = 0; row < BRICKS_IN_BASE; row++ ){
+        
+            int bricksInRow = BRICKS_IN_BASE - row;
+ 
+            for( int brickNum = 0; brickNum < bricksInRow; brickNum++ ){
+            int x = ( getWidth()/2 ) - (BRICK_WIDTH * bricksInRow) / 2 + brickNum * BRICK_WIDTH;
+            int y = getHeight() - BRICK_HEIGHT * (row+1);
+ 
+                GRect brick = new GRect( x , y , BRICK_WIDTH , BRICK_HEIGHT );
+                add(brick);
+            }
+        }
+    }
 }
-
