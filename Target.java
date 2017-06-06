5ed397c2190fd4ebc431ed072fe0a5dbb4243adf
S <s>
2017-06-05 17:07:07 -0700

    {"types":"C","runs":15}

--- Target.java
+++ Target.java
@@ -18,7 +18,7 @@ public class Target extends GraphicsProgram {
 		add(oval1);
 		
 		
-		GOval oval2 = new GOval(350, 140, 50, 50);
+		GOval oval2 = new GOval(350, 137, 50, 50);
 		//oval2.setFilled(true);
 		//oval2.setFillColor(Color.RED);
 		add(oval2);
