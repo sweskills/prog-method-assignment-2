9c85a62406be6cfd1650010c6061cf04bee4ee9c
S <s>
2017-06-07 07:06:45 +0100

    {"types":"C","runs":87}

--- Target.java
+++ Target.java
@@ -18,14 +18,14 @@ public class Target extends GraphicsProgram {
 	double outerRad = 1.0 * InchesToRadius;
 	double middleRad = 0.65 * InchesToRadius;
 	double innerRad = 0.3 * InchesToRadius;
-	double centerX = getWidth()/2;
-	double centerY = getHeight()/2;
+	double centerX = getWidth();
+	double centerY = getHeight();
 			
 	
 	public void run() {
 		
 		///
-		GOval spar = new GOval(centerX * outerRad, centerY * outerRad, 2 * outerRad, 2 * outerRad);
+		GOval spar = new GOval(2 * outerRad, 2 * outerRad);
 		spar.setColor(Color.RED);
 		spar.setFillColor(Color.RED);
 		spar.setFilled(true);
@@ -33,7 +33,7 @@ public class Target extends GraphicsProgram {
 		add (spar);
 		
 		// 
-		GOval spor = new GOval(centerX * middleRad, centerY * middleRad, 2 * middleRad, 2 * middleRad);
+		GOval spor = new GOval(2 * middleRad, 2 * middleRad);
 		spor.setColor(Color.WHITE);
 		spor.setFillColor(Color.WHITE);
 		spor.setFilled(true);
@@ -41,7 +41,7 @@ public class Target extends GraphicsProgram {
 		add (spor);
 		
 		//
-		GOval spea = new GOval(centerX * innerRad, centerY * innerRad, 2 * innerRad, 2 * innerRad);
+		GOval spea = new GOval(2 * innerRad, 2 * innerRad);
 		spea.setColor(Color.RED);
 		spea.setFillColor(Color.RED);
 		spea.setFilled(true);
