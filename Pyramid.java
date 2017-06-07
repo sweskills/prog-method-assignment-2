c483577020571ccf1ad2a192b77d111a4c497ff3
S <s>
2017-06-07 16:29:19 +0100

    {"types":"C","runs":3}

--- Pyramid.java
+++ Pyramid.java
@@ -17,13 +17,13 @@ import java.awt.*;
 public class Pyramid extends GraphicsProgram {
 
 /** Width of each brick in pixels */
-	private static final int BRICK_WIDTH = 40;
+	private static final int BRICK_WIDTH = 30;
 
 /** Width of each brick in pixels */
-	private static final int BRICK_HEIGHT = 14;
+	private static final int BRICK_HEIGHT = 12;
 
 /** Number of bricks in the base of the pyramid */
-	private static final int BRICKS_IN_BASE = 16;
+	private static final int BRICKS_IN_BASE = 14;
 	
 	public void run() {
 		/* You fill this in. */
