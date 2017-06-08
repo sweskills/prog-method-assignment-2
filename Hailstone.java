ab5a72b37b5268a56f4c1db617f1c0ded5b11c3e
S <s>
2017-06-08 09:42:01 +0100

    {"types":"C","runs":12}

--- Hailstone.java
+++ Hailstone.java
@@ -14,7 +14,7 @@ public class Hailstone extends ConsoleProgram {
 		
 		int n;
 		int i;
-		println("Enter a number number "+n);
+		println("Enter a number number ");
 		
 		n =readInt(" Number ");
 		while (n>1){
