8bf7a66f5f8e4db1ec8cd63c78b0682014679f15
S <s>
2017-06-08 09:40:01 +0100

    {"types":"C","runs":8}

--- Hailstone.java
+++ Hailstone.java
@@ -11,6 +11,36 @@ import acm.program.*;
 public class Hailstone extends ConsoleProgram {
 	public void run() {
 		/* You fill this in */
+		
+		int n;
+		int i;
+		println("Enter a number number "+);
+		
+		n =readInt(" Number ");
+		while (n>1){
+			if((n%2)==0){
+				n=(n/2);
+				println("this is even I make halfn/2" +n);
+				
+			}
+			
+			else if ((n%2)==1){
+		
+				n=((3*n)+1);
+				
+				println(" this is ond I make 3*n +1" +n);
+				
+				
+			}
+		}
+	}
+
+		
+		
+		
+		
+		
+		
 	}
 }
 
